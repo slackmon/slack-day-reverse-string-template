bc5be7bd90e9bbeb284c7e169616cf9583019912
Monica Wilkinson <mwilkinson@slack-corp.com>
2022-07-13 09:36:09 -0700

    Minor

--- functions/reverse.ts
+++ functions/reverse.ts
@@ -4,7 +4,7 @@ import type { ReverseFunction } from "../manifest.ts";
 const reverse: SlackFunctionHandler<typeof ReverseFunction.definition> = async (
   { inputs, env },
 ) => {
-  console.log(`reversing ${inputs.stringToReverse}.`);
+  console.log(`Reversing ${inputs.stringToReverse}.`);
   console.log(`SLACK_API_URL=${env["SLACK_API_URL"]}`);
 
   const reverseString = inputs.stringToReverse.split("").reverse().join("");
